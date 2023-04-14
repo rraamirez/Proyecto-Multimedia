48ea02915758cf54e3ec8e34e0d4d0c76c303ec1
rraamirez <ramirezabrilraul@gmail.com>
2023-04-14 19:23:02 +0200

    haciendo p7

---
+++ p7/Practica7/src/practica7/tipos.java
@@ -0,0 +1,16 @@
+package practica7;
+
+/**
+ *
+ * @author raul
+ */
+
+/**
+ * 
+ * Tipo de dato enumerado que nos ayudará a la hora de poder distinguir entre
+ * la forma que queremos que el método paint dibuje.
+ * 
+ */
+public enum tipos {
+    LINEA, RECTANGULO, ELIPSE
+}
